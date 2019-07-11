67d9387527601560877f0f4b8f35d947cc850669
Presto <yoohan95@gmail.com>
2019-07-11 15:57:54 +0900

    Initial Commit

---
+++ Two Sum.swift
@@ -0,0 +1,23 @@
+//
+//  Two Sum.swift
+//  LeetCode
+//
+//  Created by Presto on 11/07/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+class Solution {
+  func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    let count = nums.count
+    for i in 0..<count - 1 {
+      let first = nums[i]
+      for j in i + 1..<count {
+        let second = nums[j]
+        if first + second == target {
+          return [i, j]
+        }
+      }
+    }
+    return []
+  }
+}
---
+++ main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by Presto on 11/07/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
