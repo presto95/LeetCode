5cdf9097d2cbd34a1579e4e9e7db4cdbde86782a
Presto <yoohan95@gmail.com>
2019-07-12 17:20:13 +0900

    905

---
+++ 905. Sort Array By Parity.swift
@@ -0,0 +1,22 @@
+//
+//  905. Sort Array By Parity.swift
+//  LeetCode
+//
+//  Created by Presto on 12/07/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+/* Solution */
+
+
+private func sortArrayByParity(_ A: [Int]) -> [Int] {
+  var result = [Int]()
+  for element in A {
+    if element.isMultiple(of: 2) {
+      result.insert(element, at: 0)
+    } else {
+      result.append(element)
+    }
+  }
+  return result
+}
