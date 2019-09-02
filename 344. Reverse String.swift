546b6a2a0237d479d41e4298ae2f61e901092b8a
Presto <yoohan95@gmail.com>
2019-09-02 10:43:06 +0900

    leetcode

---
+++ 344. Reverse String.swift
@@ -0,0 +1,13 @@
+//
+//  344. Reverse String.swift
+//  LeetCode
+//
+//  Created by Presto on 02/09/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+/* Solution */
+
+private func reverseString(_ s: inout [Character]) {
+  return s.reverse()
+}
