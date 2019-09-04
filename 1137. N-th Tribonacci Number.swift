26156a344372dee5e7528982566f084950f1b91d
Presto <yoohan95@gmail.com>
2019-09-04 11:35:42 +0900

    leetcode

---
+++ 1137. N-th Tribonacci Number.swift
@@ -0,0 +1,28 @@
+//
+//  1137. N-th Tribonacci Number.swift
+//  LeetCode
+//
+//  Created by Presto on 04/09/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+private func tribonacci(_ n: Int) -> Int {
+  if n == 0 { return 0 }
+  if n == 1 { return 1 }
+  if n == 2 { return 1 }
+  
+  var first = 0
+  var second = 1
+  var third = 1
+  
+  for _ in 3...n {
+    let new = first + second + third
+    let secondToFirst = second
+    let thirdToSecond = third
+    first = secondToFirst
+    second = thirdToSecond
+    third = new
+  }
+  
+  return third
+}
