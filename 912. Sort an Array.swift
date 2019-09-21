0de63f7dbe588514c6f45b78cc341fcfdea834a8
Presto <yoohan95@gmail.com>
2019-09-21 16:45:32 +0900

    leetcode

---
+++ 912. Sort an Array.swift
@@ -0,0 +1,13 @@
+//
+//  912. Sort an Array.swift
+//  LeetCode
+//
+//  Created by Presto on 21/09/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+/* Solution */
+
+private func sortArray(_ nums: [Int]) -> [Int] {
+  return nums.sorted(by: <)
+}
